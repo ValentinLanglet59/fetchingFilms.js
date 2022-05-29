4b8fe8c4b825b15d63851407351b6b11c277df8a
ValentinLanglet59 <valdu59960@hotmail.fr>
2022-05-29 16:58:00 +0200

    fetching films working

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const results = document.querySelector("#results")
+const inputEl = document.getElementById("search-input")
+const submitEl = document.querySelector("#submit-btn")
+const form = document.querySelector("#search-form")
 
-fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
-  .then(response => response.json())
-  .then((data) => {
-    data.Search.forEach((movie) => {
-        console.log(movie)
-        results.insertAdjacentHTML("beforeend", 
-        `<li>
-        <img src="${movie.Poster}"
-        ${movie.Title}
-        </li>`) 
+
+const searchMovies = (input) => {
+    fetch(`http://www.omdbapi.com/?s=${input}&apikey=adf1f2d7`)
+    .then(response => response.json())
+      .then((data) => {
+          data.Search.forEach((movie) => {
+              results.insertAdjacentHTML("beforeend", 
+              `<li class="list-inline-item">
+              <img src="${movie.Poster}"
+              ${movie.Title}
+              </li>`) 
+            })
+        })
+}
+    
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = inputEl.value
+    searchMovies(input)
+    results.innerHTML = ""
     })
-    })
-  
+
+searchMovies("Joker")
